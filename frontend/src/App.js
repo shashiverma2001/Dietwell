b2644dbfe651695fab12c2699d940e6a91a447a3
Deepanshu44v <deepanshu2070.be21@chitkara.edu.in>
2024-07-28 23:09:55 +0530

    final deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
   const logauth = async (e) => {
     const token = localStorage.getItem('token')
     // const data = { islogin };
-    const response = await fetch("http://localhost:2000/", {
+    const response = await fetch("https://dietwell-wt6b.onrender.com/", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
