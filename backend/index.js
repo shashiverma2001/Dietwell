abb0ea342ebfd1b4b29cd24c0e32163ece7d4f33
Deepanshu Singh <92912638+Deepanshu44v@users.noreply.github.com>
2024-08-28 16:58:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,20 @@ app.use(express.json());
 app.use(cookieParser());
 // for parsing and converting data to json format
 const cors = require("cors"); // cors is user to communicating between frontend and backend bassically used for cross origin communication like our frontend is running on port no 3000 and backend is running on 3000 port no then cors is used to communicate
+const allowedOrigins = [
+  'https://deployment-9cai.onrender.com',
+  'https://deployment-npko.onrender.com',
+];
+
 const corsOptions = {
-  origin: 'https://deployment-npko.onrender.com/', //included origin as true
-  credentials: true, //included credentials as true
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow cookies and other credentials to be sent
 };
 
 app.use(cors(corsOptions));
