efeacc728cac7d371326b8dcbc7e6ad00aa5f710
Shashi Shekhar Verma <vermashashi360@gmail.com>
2025-05-14 23:40:31 +0530

    final

--- frontend/src/assets/Teammembers.js
+++ frontend/src/assets/Teammembers.js
@@ -1,18 +1,16 @@
-import sahil from "./loginImage.jpeg";
-import sahilImage from "./sahilphoto.jpeg";
-import mintuImage from "./mintuphoto.jpeg";
-import kanishkaImage from "./kanishkaphoto.jpg";
-import ayanImage from "./ayanphoto.jpg";
-import Shashi from "./Shashi.png";
+import sahil from './loginImage.jpeg'
+import sahilImage from './sahilphoto.jpeg'
+import mintuImage from './mintuphoto.jpeg'
+import ShashiImage from './shashiphoto.png'
+import Akhileshimage from './Akhilehimage.jpg'
+import Ratanimage from './Ratanimage.jpg'
+import parthimage from "/parth.jpg" 
 const teamMembers = [
-  { name: "Deepanshu Singh", role: "Developer", imageUrl: sahilImage },
-  {
-    name: "Mintu Chaudhary",
-    role: "CEO, Marketing Head",
-    imageUrl: mintuImage,
-  },
-  { name: "Shashi Verma", role: "Developer", imageUrl: Shashi },
-  { name: "Ayan Firdosh", role: "Project Manager", imageUrl: ayanImage },
+    { name: 'Ratan Rohit', role: 'Lead Developer', imageUrl: Ratanimage
+     },
+    { name: 'Akhilesh Guleria', role: 'CEO, Marketing Head', imageUrl: Akhileshimage },
+    { name: 'Shashi Verma', role: 'Lead Designer', imageUrl: ShashiImage },
+    { name: 'Parth Arora', role: 'Project Manager', imageUrl: parthimage },
 ];
 export default teamMembers;
 // this is for our team detail and is we have to add a member to our team we can add data here and team is automatically added to our about page page no need to hardcode there
